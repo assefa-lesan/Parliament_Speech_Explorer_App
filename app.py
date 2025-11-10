3cae3677e93e861e716b909341e3f22dae15c8c1
Assefa Atsbha <assefa@lesan.ai>
2025-11-10 15:00:39 +0300

    Initial working prototype with landing page and CSS

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 from fasthtml.common import *
 
-app,rt = fast_app()
+app, rt = fast_app()
 
+# ---- STATIC FILES ---
+@rt('/static/<path>')
+def static_files(req, path):
+        return static_file(path, static_dir=path(__file__).parent / "static")
+
+# ---- ROUTE 1: Landing Page ----
 @rt('/')
-def get(): return Div(P('Hello World!'), hx_get="/change")
+def get():
+    return Html(
+        Head(
+            Title("Parliament Speech Explorer"),
+            Link(rel="stylesheet", href="/static/style.css") # <-- links CSS
+        ),
+        Body(
+            Div({"class": "container hero"},
+                H1("Parliament Speech Explorer"),
+                P("A prototype to unlock Ethiopian Parliamentary Speechs using AI"),
+                A("Go to Explorer", href="/explorer", cls="btn"),
+                Footer(
+                    P("Prototype • For Journalists & Researchers • Powered by Lesan AI tools")
+                )
+            )
+        )
+    )
+
+# ---- ROUTE 2: Explorer ( TODO ) ----
+@rt('/explorer')
+def explorer():
+    return Div(
+        H2("Explorer Page"),
+        P("This will later show searchable Speeches"),
+        A("Back to Home", href="/")
+    )
 
 serve()
